8a4a081982a946120086e96f0534c42731a028f3
xiaotutu365 <550228957@qq.com>
2018-09-28 19:00:25 +0800

    init commit.

---
+++ springcloud-bus/springcloud-bus-configcenter/src/main/java/com/trey/springcloud/bus/configcenter/SpringCloudBusConfigCenterApplication.java
@@ -0,0 +1,12 @@
+package com.trey.springcloud.bus.configcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudBusConfigCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudBusConfigCenterApplication.class, args);
+    }
+}
---
+++ springcloud-bus/springcloud-bus-kafka/src/main/java/com/trey/springcloud/bus/kafka/SpringCloudBusKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.trey.springcloud.bus.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudBusKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudBusKafkaApplication.class, args);
+    }
+}
