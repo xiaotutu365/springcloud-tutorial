a69fa9827501381f59dada1e62c20a1ae0159749
xiaotutu365 <550228957@qq.com>
2018-09-29 16:15:53 +0800

    commit spring cloud bus.

--- springcloud-bus/springcloud-bus-kafka/src/main/java/com/trey/springcloud/bus/kafka/controller/ConfigController.java
+++ springcloud-bus/springcloud-bus-kafka/src/main/java/com/trey/springcloud/bus/kafka/controller/ConfigController.java
@@ -1,11 +1,13 @@
 package com.trey.springcloud.bus.kafka.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/bus/config")
 public class ConfigController {
